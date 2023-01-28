646ba356125bf7f2e85f97c95d8efa1e29bb62ea
Camila Freitas <camilafreitas.economia@gmail.com>
2023-01-28 15:07:22 -0300

    formulário1

--- core/settings.py
+++ core/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v!2cm+=18oox6l+vd2-ovg(qe7@d^-pdva7^br*zk$q^m(yd6n'
+SECRET_KEY = config('SECRET_KEY', cast=str)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+Debug = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 

53f583a048d600af4f501fd640ca21cf2e213f8a
weblerson <zonejr4@gmail.com>
2023-01-28 17:01:30 -0400

    combo box

--- app_platform/views.py
+++ app_platform/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth
 
+
+@login_required(login_url='/auth/login')
 def avaliation(request: HttpRequest):
-    ...
+    match request.method:
+        case 'GET':
+            return render(request, 'avaliacao.html')

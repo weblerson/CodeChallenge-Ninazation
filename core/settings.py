f278342be681a6499d288f638451f7941adaa819
Camila Freitas <camilafreitas.economia@gmail.com>
2023-01-28 15:12:12 -0300

    formulário2

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', cast=str)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-Debug = config('DEBUG', cast=bool)
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
